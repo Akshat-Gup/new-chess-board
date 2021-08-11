a536486038ba7e7d4d37c0c702cc76cdbe02b29e
Akshat Gupta <akshatguptabusiness@gmail.com>
2021-08-11 22:53:23 +0800

    refactored code

--- app.js
+++ app.js
@@ -1,28 +1,61 @@
 var whiteTurn = true;
+const BOARD_LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+const BLANK_IMG = '<img src="./images/blank.png">';
 
-if(document.documentElement.clientWidth <= 960) {
-	document.body.innerHTML = `
-		<h1>ChessBoard</h1>
-		<div class="capturePiece" id="piece-menu"></div>
-		<script src="app.js"></script>
-	`;
-	const boardLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-	// Adding a menu for captured pieces
+const switchTurn = () => {
+	whiteTurn = !whiteTurn;
+};
+const replaceTitle = (h1) => {
+	h1.textContent = `${whiteTurn ? "White's" : "Black's"} turn`;
+};
+const isPiece = (imgClicked) => {
+	// If it's white's turn, a playable piece must contain 1 or 2 (rows associated with white pieces)
+	// If it's black's turn, a playable piece must contain 7 or 8 (rows associated with black pieces)
+	const WHITE_ROWS = ['1', '2'];
+	const BLACK_ROWS = ['7', '8'];
+	if (whiteTurn) {
+		return WHITE_ROWS.some((el) => imgClicked.includes(el));
+	} else {
+		return BLACK_ROWS.some((el) => imgClicked.includes(el));
+	}
+};
 
-	let capturedPieces = document.createElement('div');
-	capturedPieces.classList.add('capturePiece');
-	document.body.appendChild(capturedPieces);
+const allowDrop = (ev) => ev.preventDefault();
 
-	// Adding the chessboard to the screen
+const drag = (ev) => {
+	ev.dataTransfer.setData('text', ev.target.id);
+	ev.dataTransfer.effectAllowed = 'move';
+};
 
+const drop = (ev) => {
+	let heading = document.querySelector('h1');
+	replaceTitle(heading);
+	switchTurn();
+	ev.preventDefault();
+
+	let capturedPieces = document.querySelector('.capturepiece');
+	let capturedPiece = ev.target.tagName == 'IMG';
+	if (capturedPiece) {
+		var pieceBeingReplaced = ev.target.parentNode;
+		capturedPieces.appendChild(ev.target);
+	} else {
+		var pieceBeingReplaced = ev.target;
+	}
+
+	var data = ev.dataTransfer.getData('text');
+	let movedPiece = document.getElementById(data);
+	pieceBeingReplaced.appendChild(movedPiece);
+};
+
+const createTable = () => {
 	let table = document.createElement('table');
-	let title = document.querySelector('h1');
-	// (9-i) = row & boardLetters(j-1) = column
+
+	// (9-i) = row & BOARD_LETTERS(j-1) = column
 	for (var i = 1; i <= 8; i++) {
 		var tableRow = document.createElement('tr');
 		for (var j = 1; j <= 8; j++) {
 			var columnElement = document.createElement('td');
-			let squareName = `${boardLetters[j - 1]} ${9 - i}`;
+			let squareName = `${BOARD_LETTERS[j - 1]} ${9 - i}`;
 
 			// A square is white if both row and column have the same modulus
 			if (i % 2 == j % 2) {
@@ -30,115 +63,88 @@ if(document.documentElement.clientWidth <= 960) {
 			} else {
 				columnElement.className = `black ` + squareName;
 			}
-			// If the rows are in the rangee 7-8 or 1-2, fill the squares with pieces
+			// If the rows are in the rangee 7-8 or 1-2, fill the pieceBeingMoved with pieces
 			columnElement.innerHTML =
 				i > 6 || i < 3
-					? `<img src="./images/${boardLetters[j - 1]}${9 - i}.png"/>`
-					: `<img src="./images/blank.png"/>`;
+					? `<img src="./images/${BOARD_LETTERS[j - 1]}${
+							9 - i
+					  }.png"/>`
+					: BLANK_IMG;
 			tableRow.appendChild(columnElement);
 		}
 		table.appendChild(tableRow);
 	}
-
 	document.body.appendChild(table);
+	return table;
+};
 
-	let blank = '<img src="./images/blank.png"/>';
-	var squares = ''; // Squares needs to be a var or else the code will break
-	let whiteRows = ['1','2'];
-	let blackRows = ['7','8'];
+const createCapturedPieces = () => {
+	let capturedPieces = document.createElement('div');
+	capturedPieces.classList.add('capturePiece');
+	document.body.appendChild(capturedPieces);
+	return capturedPieces;
+};
 
-	let movedIndicator = document.querySelector("#piece-menu");
-	const initialSquare = (event) => {
-		// Piece validation: Two cases:
-		let parentInnerHtml = String(event.target.parentElement.innerHTML);
-		let validRow = el => parentInnerHtml.includes(el);
-		let isWhite = whiteTurn && whiteRows.some(validRow);
-		let isBlack = !whiteTurn && blackRows.some(validRow);
-
-		if (isWhite || isBlack) {
-			squares = `${parentInnerHtml}`; // store the currenet piece in a global variable so that it can be accessed later
-			movedIndicator.innerHTML = squares;
-			whiteTurn = !whiteTurn;
-			event.target.parentElement.innerHTML = blank; // remove the image of the square
-		}
-		table.addEventListener(
-			'click',
-			(e) => {
-				if(String(e.target.tagName) == "IMG") { // preventing the user from clicking the table directly
-					isWhite || isBlack ? finalSquare(e) : initialSquare(e);
-				}
-				
-			},
-			{ once: true }
-		);
-	};
-	const finalSquare = (event) => {
-		let parentInnerHtml = String(event.target.parentElement.innerHTML);
-		if (parentInnerHtml != '<img src="./images/blank.png">'){
-			capturedPieces.innerHTML += parentInnerHtml;
-		}
-		event.target.parentElement.innerHTML = squares;
-		squares = [];
-		movedIndicator.innerHTML = '<img src="./images/blank.png">';
-		table.addEventListener(
-			'click',
-			(e) => {
-				if(String(e.target.tagName) == "IMG") {
-					initialSquare(e); // // preventing the user from clicking the table directly
-				}
-				
-			},
-			{ once: true }
-		);
-		title.textContent = whiteTurn ? "White's turn" : "Black's turn";
-	};
-	// Initial code: Listen for the initial square
+const liftPieceHandler = (event) => {
+	let imageClicked = event.target.outerHTML;
+	let validPiece = isPiece(imageClicked);
+	if (validPiece) {
+		pieceBeingMoved = `${imageClicked}`;
+		switchTurn();
+		event.target.outerHTML = BLANK_IMG; // remove the image of the square
+	}
 	table.addEventListener(
 		'click',
 		(e) => {
-			if(String(e.target.tagName) == "IMG") { // preventing users from clicking on the table directly
-				initialSquare(e);
+			if (e.target.tagName == 'IMG') {
+				validPiece ? dropPieceHandler(e) : liftPieceHandler(e);
 			}
-			
 		},
 		{ once: true }
 	);
+};
+const dropPieceHandler = (event) => {
+	let imageClicked = String(event.target.outerHTML);
+	if (imageClicked != BLANK_IMG) {
+		capturedPieces.innerHTML += imageClicked;
+	}
+	event.target.outerHTML = pieceBeingMoved;
+	replaceTitle(title);
+	table.addEventListener(
+		'click',
+		(e) => {
+			if (String(e.target.tagName) == 'IMG') {
+				liftPieceHandler(e);
+			}
+		},
+		{ once: true }
+	);
+};
 
-} else {
-	function allowDrop(ev) {
-		let whiteCase = whiteTurn && (String(ev.target.parentNode.classList).includes("1") || String(ev.target.parentNode.classList).includes("2"));
-		let blackCase = (!whiteTurn) && (String(ev.target.parentNode.classList).includes("7") || String(ev.target.parentNode.classList).includes("8"));
-		if(whiteCase || blackCase) {
-			ev.preventDefault();
-			console.log('h')
-			ev.dataTransfer.effectAllowed = "move";
-		} else {
-			ev.dataTransfer.effectAllowed = "none";
-		}
-		ev.preventDefault();
-		
-	}
-	
-	function drag(ev) {
-		ev.dataTransfer.setData("text", ev.target.id);
-		ev.dataTransfer.effectAllowed = "move";
-	}
+let onMobile = document.documentElement.clientWidth <= 960;
+if (onMobile) {
+	// Setting custom HTML if the user is on mobile
+	let mobileHtml = `
+		<h1>ChessBoard</h1>
+		<div class="capturePiece" id="piece-menu"></div>
+		<script src="app.js"></script>
+	`;
+	document.body.innerHTML = mobileHtml;
 
-	function drop(ev) {
-		ev.preventDefault();
-		var data = ev.dataTransfer.getData("text");
-		console.log(ev.target.tagName)
-		if (ev.target.tagName == "IMG") {
-			let node = ev.target.parentNode;
-			capturedPieces = document.querySelector('.capturepiece');
-			capturedPieces.appendChild(ev.target);
-			//ev.target.parentNode.replaceChildren();
-			node.appendChild(document.getElementById(data))
-		} else {
-			ev.target.appendChild(document.getElementById(data));
-		}
-		let heading = document.querySelector('h1');
-		whiteTurn = !whiteTurn;
-		heading.textContent = `${whiteTurn ? "White's" : "Black's"} turn`;
-	}
+	// The website's most important elements
+	var capturedPieces = createCapturedPieces();
+	var table = createTable();
+	var title = document.querySelector('h1');
+	var pieceBeingMoved;
+
+	table.addEventListener(
+		'click',
+		(e) => {
+			if (String(e.target.tagName) == 'IMG') {
+				// preventing users from clicking on the table directly
+				liftPieceHandler(e);
+			}
+		},
+		{ once: true }
+	);
 }
